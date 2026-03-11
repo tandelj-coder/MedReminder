87e4d7423f4a4883dc5303e38245583b45e8ef5d
Jigar Tandel <tandelj@sheridancollege.ca>
2026-03-11 11:57:47 -0400

    chore: increment MedicationDatabase version to 3

--- app/src/main/java/ca/sheridancollege/medreminder/data/local/MedicationDatabase.kt
+++ app/src/main/java/ca/sheridancollege/medreminder/data/local/MedicationDatabase.kt
@@ -9,7 +9,7 @@ import ca.sheridancollege.medreminder.data.local.entity.MedicationEntity
 
 @Database(
     entities = [MedicationEntity::class, IntakeLogEntity::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class MedicationDatabase : RoomDatabase() {
